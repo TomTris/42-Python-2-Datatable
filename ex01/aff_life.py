c577c713da793d3f6bf50515f13bf8b374829150
TomTris <quoctrido.jb@gmail.com>
2024-08-13 19:02:14 +0200

    know how to extract data and virtualize it

---
+++ ex01/aff_life.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+from load_csv import load
+
+
+def plot_country_life_expectancy(country_name: str, csv_path: str):
+    try:
+        df = load(csv_path)
+
+        if df is None:
+            return None
+        country_data = df[df['country'] == country_name]
+
+        if country_data.empty:
+            print(f"No data found for {country_name}")
+            return None
+
+        years = country_data.columns[1:].astype(int)
+        life_expectancy = country_data.iloc[0, 1:].values
+    except Exception as e:
+        print(f"An Error occurs: {e}")
+        return None
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(years, life_expectancy, marker='o', color='b', label=country_name)
+    plt.xlabel("Year")
+    plt.ylabel("life_expectancy")
+    plt.title(f"Life expectancy over time for {country_name}")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+def main():
+    return 1
+
+
+if __name__ == "__main__":
+    main()
---
+++ ex01/tester.py
@@ -0,0 +1,2 @@
+from aff_life import plot_country_life_expectancy
+plot_country_life_expectancy("Germany", "life_expectancy_years.csv")
